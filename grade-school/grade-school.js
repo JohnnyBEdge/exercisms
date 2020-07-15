c99a56e5e4bd121dba476fe87e4d6caf70fa7b09
John Martin <jrmartin182@gmail.com>
2020-07-15 10:18:12 -0600

    completed solution

--- grade-school/grade-school.js
+++ grade-school/grade-school.js
@@ -2,20 +2,22 @@
 // This is only a SKELETON file for the 'Grade School' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// let roster;
 
 export class GradeSchool {
-  
-
-
+  constructor(){
+    this._roster = {};
+  }
   roster() {
-    
+    return JSON.parse(JSON.stringify(this._roster));
   }
 
-  add() {
-    throw new Error("Remove this statement and implement this function");
+  add(student, grade) {
+    this._roster[grade] ? this._roster[grade].push(student) : this._roster[grade] = [student];
+    return this._roster[grade].sort();
   }
 
-  grade() {
-    throw new Error("Remove this statement and implement this function");
+  grade(grade) {
+    return JSON.parse(JSON.stringify(this._roster[grade] || []));
   }
 }
